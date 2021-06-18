e694f206c053461141ce76f43205def289a24721
Nasim Bilal <71866414+NasimBilal@users.noreply.github.com>
2021-06-18 15:21:57 +0530

    Add files via upload

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -10,7 +10,7 @@ function Banner() {
     const [banner_id, setbanner_id] = useState('')
     useEffect(()=>{
         axios.get("https://api.themoviedb.org/3/trending/all/day?api_key=29be00fe03756dac4071acdd3d77e965").then(response=>{    
-        setState(response.data.results[10])
+        setState(response.data.results[1])
         })
     },[])
     const banner_trailer=(id)=>{
@@ -29,11 +29,9 @@ function Banner() {
     return (
         <div style = {{backgroundImage:`url(${state ? image_api +state.backdrop_path : ""})` }} className="banner">
             <div className="content">
-                
                 <h1 className="title">{state ? state.title : ""}</h1>
                 <div className="banner_buttons">
-                    <button onClick={()=>banner_trailer(state.id)} className="button" >Watch</button>
-                    <button className="button">Add To MyList</button>
+                    <button onClick={()=>banner_trailer(state.id)} className="button" >Watch Trailer</button>
                 </div>
                 <h1 className="description">{state ? state.overview : ""}</h1>
             </div>
@@ -43,8 +41,6 @@ function Banner() {
             {banner_id && <Youtube  videoId={banner_id.key} opts={opts}/>}
             </div>
         </div>
-        
-        
     )
 }
 
